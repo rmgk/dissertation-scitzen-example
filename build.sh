9657bf2f84698d11f2cd5fa35c1567e57375883f
ragnar <git@rmgk.de>
2023-06-30 20:28:54 +0200

    add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/sh
+
+if command -v tectonic > /dev/null
+then
+	:
+else
+	echo "tectonic not found in path, will likely fail to compile PDFs, install here: https://tectonic-typesetting.github.io/en-US/"
+fi
+
+if command -v cs > /dev/null
+then
+	cs launch --repository jitpack de.rmgk:scitzen:967e2cca6f -- .
+else
+	echo "please install https://get-coursier.io/docs/cli-overview"
+fi
+
+cp -R templates/reveal/ out/web/
+
+# cs bootstrap --repository jitpack de.rmgk:scitzen:967e2cca6f --main-class scitzen.cli.ScitzenCommandline -f -o build
+
